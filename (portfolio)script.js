c6d9f2e4f45c35b37ed1f97af5100014989b3d4e
Mithra J <mithraacct@gmail.com>
2024-12-02 23:33:22 +0530

    Create (portfolio)script.js

---
+++ (portfolio)script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.getElementById('nav-menu');
+    
+    // Toggle navigation menu visibility
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active'); // Adds or removes 'active' class to nav
+    });
+});
